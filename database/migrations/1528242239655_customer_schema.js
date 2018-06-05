cdb5345a92eef15efde7ebf6ed5050e127c493fe
Dendi Handian <dendihandian@gmail.com>
2018-06-06 06:44:32 +0700

    add Customer model, controller, migration

---
+++ database/migrations/1528242239655_customer_schema.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class CustomerSchema extends Schema {
+  up () {
+    this.create('customers', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('customers')
+  }
+}
+
+module.exports = CustomerSchema
